19044edf9351dba3584b11cf258005974a730084
David Yim <yimd85@gmail.com>
2018-11-12 19:18:19 -0500

    added 2 test suites, added env for absolute pathing

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-    return<div>Comment Box</div>;
+class CommentBox extends Component {
+    state = {
+        comment: '',
+    };
+    
+    handleChange = event => {
+        this.setState({ comment: event.target.value });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({ comment: ' ' })
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <h4> Add a Comment</h4>
+                <textarea
+                    onChange={this.handleChange} 
+                    value={this.state.comment}
+                />
+                <div>
+                    <button>Submit Button</button>
+                </div>
+            </form>
+        );
+
+    }
+
 }
+
+
+
+export default CommentBox;
+
+//transition this from a function based component to a class based component 
+//this way this can have component level state and have call back
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
-import CommentBox from '../CommentBox';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App />);
+});
+
+//The before each applies to every test inside this FILE
 
 it('shows a comment box', () => {
-    const div = document.createElement('div');
+    expect(wrapped.find(CommentBox).length).toEqual(1);
 
-    ReactDOM.render(<App />, div);
+    // const div = document.createElement('div');
+    // ReactDOM.render(<App />, div);
 
     // Looks inside the div and checsk to see if the CommentBox is in there
     //to run tst write out npm run test
 
     // console.log(div.innerHTML);
     // expect(div.innerHTML).toContain('Comment Box');
-
     // expect(div).toHaveAnInstanceOf(CommentBox);
 
-    ReactDOM.unmountComponentAtNode(div); //find the div component and remove the App component. This is referred to as "Clean Up"
+    // ReactDOM.unmountComponentAtNode(div); 
+    //find the div component and remove the App component. This is referred to as "Clean Up"
     //destorys any component/objects. 
+});
+
+it('shows a component box', () => {
+    expect(wrapped.find(CommentList).length).toEqual(1);
 });
