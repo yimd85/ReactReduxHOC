13816a349d033186ecbbaa39f95cf5439af29bf7
David Yim <yimd85@gmail.com>
2018-11-24 09:25:09 -0500

    added stuff

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -11,9 +11,8 @@ class CommentList extends Component {
     render() {
         return (
             <div>
-                <ul>
-                    {this.renderComments()}
-                </ul>
+                <h4>Comment List</h4>
+                <ul>{this.renderComments()}</ul>
             </div>
         );
     }
